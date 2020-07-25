312beee7e4ef9ad10915c1bf3b7bda99d6e6342f
Jose Javier Rodriguez Gallego <sngular_jjrodriguez@sacyr.com>
2020-07-26 00:47:27 +0200

    change loader animation

--- EAPN.HDVS.Web/ClientApp/src/app/shared/modules/loader/loader.module.ts
+++ EAPN.HDVS.Web/ClientApp/src/app/shared/modules/loader/loader.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [LoaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   exports: [LoaderComponent],
 })
 export class LoaderModule {}
