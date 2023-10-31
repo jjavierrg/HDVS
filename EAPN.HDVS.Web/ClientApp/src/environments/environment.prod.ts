8bd2874f021efbefbd458ed95a9ac46318fccbb4
Javier Rodriguez <Javier.Rodriguez@azure.techlensglobal.com>
2023-10-31 11:51:46 +0100

    ci: :construction_worker: Using environment in actions

--- EAPN.HDVS.Web/ClientApp/src/environments/environment.prod.ts
+++ EAPN.HDVS.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+import { env } from 'process';
+
 export const environment = {
   production: true,
-  apiEndpoint: 'https://hdvs.jrgallego.com',
+  apiEndpoint: env.API_ENDPOINT,
   tokenExcludeEndpoints: ['api/auth/refresh', 'api/auth'],
   tokenLocalStorageKey: 'HDVSApiToken',
 };
